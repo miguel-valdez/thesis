fa30d2a5d22be398f68f8ca628940581b9a41788
miguel <miguel.valdesg@gmail.com>
2018-01-13 02:04:31 +0000

    last commit? need last proofread!

--- build_thesis.sh
+++ build_thesis.sh
@@ -3,12 +3,12 @@
 pdflatex thesis.tex
 bibtex thesis.aux
 
-bibtex intro/intro.aux
-bibtex research-1/research-1.aux
-bibtex research-2/research-2.aux
-bibtex research-3/research-3.aux
-bibtex research-4/research-4.aux
-bibtex conclusions/conclusions.aux
+#bibtex intro/intro.aux
+#bibtex research-1/research-1.aux
+#bibtex research-2/research-2.aux
+#bibtex research-3/research-3.aux
+#bibtex research-4/research-4.aux
+#bibtex conclusions/conclusions.aux
 
 pdflatex thesis.tex
 pdflatex thesis.tex
--- clean.sh
+++ clean.sh
@@ -3,6 +3,14 @@
 rm -v ./*.aux
 rm -v ./*.bbl
 rm -v ./*.blg
+rm -v ./*.ilg
+rm -v ./*.log
+rm -v ./*.lot
+rm -v ./*.lof
+rm -v ./*.toc
+rm -v ./*.nls
+rm -v ./*.nlo
+rm -v ./*.out
 
 rm -v ./*/*.aux
 rm -v ./*/*.bbl
@@ -10,3 +18,9 @@ rm -v ./*/*.blg
 
 rm -v ./*~
 rm -v ./*/*~
+
+
+
+thesis.bbl  thesis.lof  thesis.nlo  thesis.pdf    thesis.toc
+build_thesis.sh  ImpStyle.sty  nomenclature.sh  preamble.tex  research-1      research-4  thesis.blg  thesis.log  thesis.nls  #thesis.tex#
+clean.sh         intro         pre              README.md     research-2      thesis.aux  thesis.ilg  thesis.lot  thesis.out  thesis.tex
